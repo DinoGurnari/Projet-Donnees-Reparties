ad7c0c4047a97af6b18d4c193877afa10de94787
Dino Gurnari <dinogurnari@gmail.com>
2022-01-14 15:15:09 +0100

    Modif script

--- compilation.sh
+++ compilation.sh
@@ -1,4 +1,4 @@
 # Simple script pour compiler les .java et supprimer les .class
 
-alias compiler="javac linda/*.java linda/*/*.java"
-alias supprimer="rm linda/*.class linda/*/*.class"
+alias compiler="javac linda/*.java linda/*/*.java; echo 'Compilation effectuée !'"
+alias supprimer="rm linda/*.class linda/*/*.class; echo 'Suppression des fichiers de compilation effectuée !'"
