0450f1e539ed6bc9bdf4aa03fa5ff0e2783f059e
Adrien Chevallereau <93998883+AdrienN7@users.noreply.github.com>
2022-01-16 00:48:42 +0100

    modif compilation pour recherche

--- compilation.sh
+++ compilation.sh
@@ -1,4 +1,4 @@
 # Simple script pour compiler les .java et supprimer les .class
 
-alias compiler="javac linda/*.java linda/*/*.java; echo 'Compilation effectuée !'"
-alias supprimer="rm linda/*.class linda/*/*.class; echo 'Suppression des fichiers de compilation effectuée !'"
+alias compiler="javac linda/*.java linda/*/*.java linda/*/*/*.java; echo 'Compilation effectuée !'"
+alias supprimer="rm linda/*.class linda/*/*.class linda/*/*/*.class; echo 'Suppression des fichiers de compilation effectuée !'"
